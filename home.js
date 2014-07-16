7d7f0e886b6933a4f5b124d7c49970b6a1d8c993
Gesang <gesarlhamo80@gmail.com>
2014-07-16 17:25:00 -0500

    added some style

--- home.js
+++ home.js
@@ -1,14 +1,28 @@
 
+var points = 0;
+var score = $("h2");
+
 $(".game li").click(function () {
 
   /* Toggle the flip class */
   $(this).toggleClass("flipped");
+
+  var flipped = $(".flipped");
+  if(flipped.length === 2){
+  	var firstCard = flipped.first();
+  	var scondCard = flipped.last();
+
+  	if(firstCard.url() === secondCard.url()){
+  		points++;
+  		score.text("you've found" + point + " matches.")
+  	}
   
   setTimeout(function () {
-     console.log("This message will appear after 500 milliseconds");
-  }, 2000);
+     $(".flipped").removeClass("flipped");
+  }, 1000);
+}
   
-  return false;
+  
 });
 
 
