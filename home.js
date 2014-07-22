e736aa41a9e01f6c0707197c8ab5ed70a5e9f8ad
Gesang <gesarlhamo80@gmail.com>
2014-07-22 11:47:30 -0500

    used jquery to select the src attr of img in the DOM and added logic to increment when a match is found

--- home.js
+++ home.js
@@ -1,28 +1,58 @@
 
 var points = 0;
 var score = $("h2");
+var bothPaths = [];
+
+var flipped = $(".flipped");
 
 $(".game li").click(function () {
+
 
   /* Toggle the flip class */
   $(this).toggleClass("flipped");
 
-  var flipped = $(".flipped");
-  if(flipped.length === 2){
-  	var firstCard = flipped.first();
-  	var scondCard = flipped.last();
+  
+  url = ($(this).children().attr('src'));
+  bothPaths.push(url);
+  if (bothPaths.length === 2){
+     compare();
+     bothPaths = [];
 
-  	if(firstCard.url() === secondCard.url()){
-  		points++;
-  		score.text("you've found" + point + " matches.")
-  	}
-  
-  setTimeout(function () {
-     $(".flipped").removeClass("flipped");
-  }, 1000);
-}
-  
-  
+  }
+ 
 });
+
+
+ 
+  // //if(flipped.length === 2){
+  // 	var firstCard = flipped.first();
+  // 	var scondCard = flipped.last();
+    
+
+  // 	if(firstCard.attr('src') === scondCard.attr('src')){
+    
+  // 		points++;
+  // 		score.text("you've found " + points + " matches.")
+  //     alert("clicked");
+  // 	} else {
+
+  //     alert("they dont match");
+  // //}
+  
+  
+  
+  
+
+
+
+function compare(){
+  console.log(bothPaths)
+  if(bothPaths[0] === bothPaths[1]){
+    points++;
+    score.text("you've found " + points + " matches.")
+  } else {
+    alert("they dont match.");
+  }
+}
 
 
